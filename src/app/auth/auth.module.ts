5068b87018b1bf8e46853ec210a80799421fc7db
Kiran Anand <liteninkiran@hotmail.com>
2023-11-06 14:49:28 +0000

    Format code

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,14 +1,12 @@
-import {ModuleWithProviders, NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {LoginComponent} from './login/login.component';
-import {MatCardModule} from "@angular/material/card";
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { MatCardModule } from "@angular/material/card";
 import { MatInputModule } from "@angular/material/input";
-import {RouterModule} from "@angular/router";
-import {ReactiveFormsModule} from "@angular/forms";
-import {MatButtonModule} from "@angular/material/button";
-import { StoreModule } from '@ngrx/store';
-import {AuthService} from "./auth.service";
-import { EffectsModule } from '@ngrx/effects';
+import { RouterModule } from "@angular/router";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { AuthService } from "./auth.service";
 
 @NgModule({
     imports: [
@@ -17,19 +15,19 @@ import { EffectsModule } from '@ngrx/effects';
         MatCardModule,
         MatInputModule,
         MatButtonModule,
-        RouterModule.forChild([{path: '', component: LoginComponent}]),
+        RouterModule.forChild([{ path: '', component: LoginComponent }]),
 
     ],
     declarations: [LoginComponent],
-    exports: [LoginComponent]
+    exports: [LoginComponent],
 })
 export class AuthModule {
     static forRoot(): ModuleWithProviders<AuthModule> {
         return {
             ngModule: AuthModule,
             providers: [
-              AuthService
-            ]
+                AuthService,
+            ],
         }
     }
 }
--- src/app/courses/courses-card-list/courses-card-list.component.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.ts
@@ -1,13 +1,13 @@
-import {Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation} from '@angular/core';
-import {Course} from "../model/course";
-import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
-import {EditCourseDialogComponent} from "../edit-course-dialog/edit-course-dialog.component";
-import {defaultDialogConfig} from '../shared/default-dialog-config';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Course } from "../model/course";
+import { MatDialog } from "@angular/material/dialog";
+import { EditCourseDialogComponent } from "../edit-course-dialog/edit-course-dialog.component";
+import { defaultDialogConfig } from '../shared/default-dialog-config';
 
 @Component({
     selector: 'courses-card-list',
     templateUrl: './courses-card-list.component.html',
-    styleUrls: ['./courses-card-list.component.css']
+    styleUrls: ['./courses-card-list.component.css'],
 })
 export class CoursesCardListComponent implements OnInit {
 
@@ -18,41 +18,32 @@ export class CoursesCardListComponent implements OnInit {
     courseChanged = new EventEmitter();
 
     constructor(
-      private dialog: MatDialog ) {
+        private dialog: MatDialog) {
     }
 
     ngOnInit() {
 
     }
 
-    editCourse(course:Course) {
+    editCourse(course: Course) {
 
         const dialogConfig = defaultDialogConfig();
 
         dialogConfig.data = {
-          dialogTitle:"Edit Course",
-          course,
-          mode: 'update'
+            dialogTitle: "Edit Course",
+            course,
+            mode: 'update'
         };
 
         this.dialog.open(EditCourseDialogComponent, dialogConfig)
-          .afterClosed()
-          .subscribe(() => this.courseChanged.emit());
+            .afterClosed()
+            .subscribe(() => this.courseChanged.emit());
 
     }
 
-  onDeleteCourse(course:Course) {
+    onDeleteCourse(course: Course) {
 
 
-  }
+    }
 
 }
-
-
-
-
-
-
-
-
-
