42d1202054b0bfce69c0a5bd526f749fea2f419e
Kiran Anand <liteninkiran@hotmail.com>
2023-11-11 22:27:17 +0000

    042 - Querying Store Data with NgRx Data and the entities$ Observable

--- src/app/courses/home/home.component.ts
+++ src/app/courses/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { compareCourses, Course } from '../model/course';
+
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { Course } from '../model/course';
 import { defaultDialogConfig } from '../shared/default-dialog-config';
 import { EditCourseDialogComponent } from '../edit-course-dialog/edit-course-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
-import { map, shareReplay } from 'rxjs/operators';
-import { CoursesHttpService } from '../services/courses-http.service';
+import { CourseEntityService } from '../services/course-entity.service';
 
 @Component({
     selector: 'home',
@@ -21,53 +23,27 @@ export class HomeComponent implements OnInit {
 
     constructor(
         private dialog: MatDialog,
-        private coursesHttpService: CoursesHttpService) {
+        private coursesService: CourseEntityService,
+    ) {
 
     }
 
-    ngOnInit() {
+    public ngOnInit(): void {
         this.reload();
     }
 
-    reload() {
-
-        const courses$ = this.coursesHttpService.findAllCourses()
-            .pipe(
-                map(courses => courses.sort(compareCourses)),
-                shareReplay()
-            );
-
-        this.loading$ = courses$.pipe(map(courses => !!courses));
-
-        this.beginnerCourses$ = courses$
-            .pipe(
-                map(courses => courses.filter(course => course.category == 'BEGINNER'))
-            );
-
-
-        this.advancedCourses$ = courses$
-            .pipe(
-                map(courses => courses.filter(course => course.category == 'ADVANCED'))
-            );
-
-        this.promoTotal$ = courses$
-            .pipe(
-                map(courses => courses.filter(course => course.promo).length)
-            );
-
+    public reload(): void {
+        this.beginnerCourses$ = this.coursesService.entities$.pipe(map(courses => courses.filter(course => course.category == 'BEGINNER')));
+        this.advancedCourses$ = this.coursesService.entities$.pipe(map(courses => courses.filter(course => course.category == 'ADVANCED')));
+        this.promoTotal$ = this.coursesService.entities$.pipe(map(courses => courses.filter(course => course.promo).length));
     }
 
-    onAddCourse() {
-
+    public onAddCourse(): void {
         const dialogConfig = defaultDialogConfig();
-
         dialogConfig.data = {
             dialogTitle: 'Create Course',
             mode: 'create'
         };
-
         this.dialog.open(EditCourseDialogComponent, dialogConfig);
-
     }
-
 }
