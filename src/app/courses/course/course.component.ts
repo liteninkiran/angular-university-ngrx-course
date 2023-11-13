f70c34cb8f1859ccbc6e6455c3190b986f2134d9
Kiran Anand <liteninkiran@hotmail.com>
2023-11-13 14:04:56 +0000

    047 - Implementing the Course Component Using NgRx Data

--- src/app/courses/course/course.component.ts
+++ src/app/courses/course/course.component.ts
@@ -1,39 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Course } from '../model/course';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { Lesson } from '../model/lesson';
-import { concatMap, tap } from 'rxjs/operators';
+import { concatMap, map, tap } from 'rxjs/operators';
 import { CoursesHttpService } from '../services/courses-http.service';
+import { CourseEntityService } from '../services/course-entity.service';
+import { LessonEntityService } from '../services/lesson-entity.service';
 
 @Component({
     selector: 'course',
     templateUrl: './course.component.html',
-    styleUrls: ['./course.component.css']
+    styleUrls: ['./course.component.css'],
 })
 export class CourseComponent implements OnInit {
 
-    course$: Observable<Course>;
-    lessons$: Observable<Lesson[]>;
-    displayedColumns = ['seqNo', 'description', 'duration'];
-    nextPage = 0;
+    public course$: Observable<Course>;
+    public lessons$: Observable<Lesson[]>;
+    public displayedColumns = ['seqNo', 'description', 'duration'];
+    public nextPage = 0;
 
     constructor(
-        private coursesService: CoursesHttpService,
-        private route: ActivatedRoute) {
+        private coursesService: CourseEntityService,
+        private lessonsService: LessonEntityService,
+        private route: ActivatedRoute,
+    ) { }
 
-    }
-
-    ngOnInit() {
+    public ngOnInit(): void {
         const courseUrl = this.route.snapshot.paramMap.get('courseUrl');
-        this.course$ = this.coursesService.findCourseByUrl(courseUrl);
-        this.lessons$ = this.course$.pipe(
-            concatMap(course => this.coursesService.findLessons(course.id)),
-            //tap(console.log)
+        this.course$ = this.coursesService.entities$.pipe(
+            map(courses => courses.find(course => course.url === courseUrl))
         );
+        this.lessons$ = of([]);
     }
 
-    loadLessonsPage(course: Course) {
+    public loadLessonsPage(course: Course): void {
 
     }
 }
