546ed0cec9e30dad56a12cf33ba3617294e2c7d2
Kiran Anand <liteninkiran@hotmail.com>
2023-11-13 00:17:28 +0000

    043 - NgRx Data CRUD - Why use Optimistic Updates?

--- src/app/courses/edit-course-dialog/edit-course-dialog.component.ts
+++ src/app/courses/edit-course-dialog/edit-course-dialog.component.ts
@@ -3,7 +3,8 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Course } from '../model/course';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { CoursesHttpService } from '../services/courses-http.service';
+import { IDialogData } from '../courses-card-list/courses-card-list.component';
+import { CourseEntityService } from '../services/course-entity.service';
 
 @Component({
     selector: 'course-dialog',
@@ -12,56 +13,49 @@ import { CoursesHttpService } from '../services/courses-http.service';
 })
 export class EditCourseDialogComponent {
 
-    form: FormGroup;
-    dialogTitle: string;
-    course: Course;
-    mode: 'create' | 'update';
-    loading$: Observable<boolean>;
+    public form: FormGroup;
+    public loading$: Observable<boolean>;
+    public data: IDialogData;
 
     constructor(
         private fb: FormBuilder,
         private dialogRef: MatDialogRef<EditCourseDialogComponent>,
-        @Inject(MAT_DIALOG_DATA) data,
-        private coursesService: CoursesHttpService,
+        private coursesService: CourseEntityService,
+        @Inject(MAT_DIALOG_DATA) data: IDialogData,
     ) {
-
-        this.dialogTitle = data.dialogTitle;
-        this.course = data.course;
-        this.mode = data.mode;
-
+        this.data = data;
         const formControls = {
             description: ['', Validators.required],
             category: ['', Validators.required],
             longDescription: ['', Validators.required],
-            promo: ['', []]
+            promo: ['', []],
         };
 
-        if (this.mode == 'update') {
+        if (this.data.mode == 'update') {
             this.form = this.fb.group(formControls);
             this.form.patchValue({ ...data.course });
         }
-        else if (this.mode == 'create') {
+        else if (this.data.mode == 'create') {
             this.form = this.fb.group({
                 ...formControls,
                 url: ['', Validators.required],
-                iconUrl: ['', Validators.required]
+                iconUrl: ['', Validators.required],
             });
         }
     }
 
-    onClose() {
+    public onClose(): void {
         this.dialogRef.close();
     }
 
-    onSave() {
-
+    public onSave(): void {
         const course: Course = {
-            ...this.course,
-            ...this.form.value
+            ...this.data.course,
+            ...this.form.value,
         };
-
-        this.coursesService
-            .saveCourse(course.id, course)
-            .subscribe(() => this.dialogRef.close());
+        if (this.data.mode === 'update') {
+            this.coursesService.update(course);
+            this.dialogRef.close();
+        }
     }
 }
