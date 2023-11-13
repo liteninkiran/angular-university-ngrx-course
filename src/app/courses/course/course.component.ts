edaed4f1f9e58e59ef584bd2a5baf4a25a94bf38
Kiran Anand <liteninkiran@hotmail.com>
2023-11-13 14:34:57 +0000

    048 - Lessons Pagination using NgRx Data

--- src/app/courses/course/course.component.ts
+++ src/app/courses/course/course.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Course } from '../model/course';
 import { Observable, of } from 'rxjs';
 import { Lesson } from '../model/lesson';
-import { concatMap, map, tap } from 'rxjs/operators';
+import { concatMap, delay, map, tap, withLatestFrom } from 'rxjs/operators';
 import { CoursesHttpService } from '../services/courses-http.service';
 import { CourseEntityService } from '../services/course-entity.service';
 import { LessonEntityService } from '../services/lesson-entity.service';
@@ -15,10 +15,12 @@ import { LessonEntityService } from '../services/lesson-entity.service';
 })
 export class CourseComponent implements OnInit {
 
+    public loading$: Observable<boolean>;
     public course$: Observable<Course>;
     public lessons$: Observable<Lesson[]>;
     public displayedColumns = ['seqNo', 'description', 'duration'];
     public nextPage = 0;
+
 
     constructor(
         private coursesService: CourseEntityService,
@@ -31,10 +33,24 @@ export class CourseComponent implements OnInit {
         this.course$ = this.coursesService.entities$.pipe(
             map(courses => courses.find(course => course.url === courseUrl))
         );
-        this.lessons$ = of([]);
+        this.lessons$ = this.lessonsService.entities$.pipe(
+            withLatestFrom(this.course$),
+            tap(([lessons, course]) => this.nextPage === 0 ? this.loadLessonsPage(course) : this.noop()),
+            map(([lessons, course]) => lessons.filter(lesson => lesson.courseId === course.id)),
+        );
+        this.loading$ = this.lessonsService.loading$.pipe(delay(0));
     }
 
     public loadLessonsPage(course: Course): void {
+        this.lessonsService.getWithQuery({
+            courseId: course.id.toString(),
+            pageNumber: this.nextPage.toString(),
+            pageSize: 3,
+        });
+        this.nextPage +=1;
+    }
+
+    public noop() {
 
     }
 }
